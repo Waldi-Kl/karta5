b8316552afcc0a93182cf04d9f0524035ca0124c
Waldi <Waldi@Waldi-HP.localdomain>
2018-03-19 19:38:33 +0100

    To jest utworzony na moik kompie projekt

---
+++ src/main/java/com/waldi/karta/controller/MainController.java
@@ -0,0 +1,63 @@
+package com.waldi.karta.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class MainController {
+
+   @RequestMapping(value = { "/", "/homepage"}, method = RequestMethod.GET)
+   public String MainController(Model model) {
+       model.addAttribute("title", "Welcome");
+       model.addAttribute("message", "Witamy na stronie powitalnej!");
+       return "homePage";
+   }
+ 
+//   @RequestMapping(value = "/admin", method = RequestMethod.GET)
+//   public String adminPage(Model model) {
+//	   model.addAttribute("title", "Admin");
+//       return "adminPage";
+//   }
+ 
+   @RequestMapping(value = "/login", method = RequestMethod.GET)
+   public String loginPage(Model model ) {
+	   model.addAttribute("title", "Login");
+       return "loginPage";
+   }
+ 
+   @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+   public String logoutSuccessfulPage(Model model) {
+       model.addAttribute("title", "Logout");
+       model.addAttribute("message", "OK!");
+       return "logoutSuccessfulPage";
+   }
+ 
+//   @RequestMapping(value = "/userInfo", method = RequestMethod.GET)
+//   public String userInfo(Model model, Principal principal) {
+// 
+//       // After user login successfully.
+//       String userName = principal.getName();
+// 
+//       model.addAttribute("title", "UserInfo");
+// 
+//       return "userInfoPage";
+//   }
+ 
+   @RequestMapping(value = "/403", method = RequestMethod.GET)
+   public String accessDenied(Model model, Principal principal) {
+        
+       if (principal != null) {
+           model.addAttribute("message", "Witaj " + principal.getName()
+                   + "<br> Nie posiadasz uprawniê do tej strony!");
+       } else {
+           model.addAttribute("msg",
+                   "Nie posiadasz uprawniê do tej strony!");
+       }
+       return "403Page";
+   }
+}
---
+++ src/main/java/com/waldi/karta/dao/UserInfoDAO.java
@@ -0,0 +1,18 @@
+package com.waldi.karta.dao;
+
+import java.util.List;
+
+import com.waldi.model.UserInfo;
+
+public interface UserInfoDAO {
+
+  //  public UserInfo getUserInfo(String userName);
+    public UserInfo getUserInfo(String userLogin);
+    
+    // [USER,AMIN,..]
+    public List<String> getUserRoles(String userName);
+    
+    public List<UserInfo> getUsersList();
+     
+
+}
